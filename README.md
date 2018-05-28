0400c9ab26994bf6820333892e517cc96e63d20b
Solange Umuhire GASENGAYIRE <sgasengayire@gmail.com>
2018-05-28 10:46:22 +0200

    Add LICENSE and README files

---
+++ README.md
@@ -0,0 +1,32 @@
+# Product Ordering Interface
+
+[![GNU General Public License v3.0](https://img.shields.io/badge/license-GPL%20v3.0-green.svg)][1]
+[![Java Back-End](https://img.shields.io/badge/back--end-Java-blue.svg)][2]
+[![JavaScript Front-End](https://img.shields.io/badge/front--end-JavaScript-F4D03F.svg)][3]
+
+Create a **web page for ordering products**:
+- a user can select	a product from the provided product	catalog	and	submit an order
+- the product catalog must also	display	prices of products (in addition	to the product information).
+
+A product order	is sent	to API and stored to the database. Orders are anonymous; no authentication or authorization of 
+a user is required. Basic information about	a customer should be saved with	the	order.
+
+**The final project should include:**
+- a HTML5/JS/CSS web interface for ordering	products: controls for displaying available	products with prices 
+and an action for submitting the order	
+- API implemented using Java 8 and provided	frameworks
+- tests
+
+
+## Languages - Libraries - Tools
+
+- [Java SE 8][4]
+- [Maven 3.3+][5]
+- Any IDE that supports JDK 8
+
+
+[1]: https://github.com/SolangeUG/conference-central/blob/master/LICENSE
+[2]: http://java.com/en/
+[3]: https://www.javascript.com/
+[4]: http://www.oracle.com/technetwork/java/javase/downloads/jdk8-downloads-2133151.html
+[5]: https://maven.apache.org/download.cgi
--- src/main/java/elisa/devtest/endtoend/dao/OrderDao.java
+++ src/main/java/elisa/devtest/endtoend/dao/OrderDao.java
@@ -12,7 +12,12 @@ public class OrderDao {
 
     public List<Order> findOrders() {
         try {
-            return createJdbcTemplate().query("select * from orders", (resultSet, rowNumber) -> new Order(resultSet.getLong("order_id"), findCustomer(resultSet.getLong("customer_id")), findOrderLines(resultSet.getLong("order_id"))));
+            return createJdbcTemplate()
+                    .query("select * from orders",
+                            (resultSet, rowNumber) -> new Order(resultSet.getLong("order_id"),
+                                                                findCustomer(resultSet.getLong("customer_id")),
+                                                                findOrderLines(resultSet.getLong("order_id")))
+                    );
         } catch (Exception e) {
             e.printStackTrace();
         }
@@ -21,7 +26,14 @@ public class OrderDao {
 
     private List<OrderLine> findOrderLines(long orderId) {
         try {
-            return createJdbcTemplate().query("select * from order_line where order_id = ?", new Object[]{orderId}, (resultSet, rowNumber) -> new OrderLine(resultSet.getLong("order_line_id"), resultSet.getLong("product_id"), resultSet.getString("product_name"), resultSet.getInt("quantity")));
+            return createJdbcTemplate()
+                    .query("select * from order_line where order_id = ?",
+                            new Object[]{orderId},
+                            (resultSet, rowNumber) -> new OrderLine(resultSet.getLong("order_line_id"),
+                                                                    resultSet.getLong("product_id"),
+                                                                    resultSet.getString("product_name"),
+                                                                    resultSet.getInt("quantity"))
+                    );
         } catch (Exception e) {
             e.printStackTrace();
         }
@@ -30,7 +42,16 @@ public class OrderDao {
 
     public Customer findCustomer(final long customerId) {
         try {
-            return createJdbcTemplate().queryForObject("select * from customer where customer_id = ?", new Object[]{customerId}, (resultSet, rowNumber) -> new Customer(resultSet.getLong("customer_id"), resultSet.getString("company_name"), resultSet.getString("street"), resultSet.getString("postal_code"), resultSet.getString("city"), resultSet.getString("country")));
+            return createJdbcTemplate()
+                    .queryForObject("select * from customer where customer_id = ?",
+                                    new Object[]{customerId},
+                                    (resultSet, rowNumber) -> new Customer(resultSet.getLong("customer_id"),
+                                                                            resultSet.getString("company_name"),
+                                                                            resultSet.getString("street"),
+                                                                            resultSet.getString("postal_code"),
+                                                                            resultSet.getString("city"),
+                                                                            resultSet.getString("country"))
+                    );
         } catch (Exception e) {
             e.printStackTrace();
         }
