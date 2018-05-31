a5bf482c159f9464b161cbb4c8a56b87dbabe797
Solange Umuhire GASENGAYIRE <sgasengayire@gmail.com>
2018-05-31 18:48:54 +0200

    Add createOrder as a POST method on the /orders path

--- src/main/java/elisa/devtest/endtoend/OrderResource.java
+++ src/main/java/elisa/devtest/endtoend/OrderResource.java
@@ -1,21 +1,34 @@
 package elisa.devtest.endtoend;
 
+import elisa.devtest.endtoend.exception.OrderProcessingException;
 import elisa.devtest.endtoend.model.Order;
 import elisa.devtest.endtoend.service.OrderService;
 
-import javax.ws.rs.GET;
-import javax.ws.rs.Path;
-import javax.ws.rs.Produces;
+import javax.validation.constraints.NotNull;
+import javax.ws.rs.*;
 import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
 import java.util.Collection;
 
 @Path("/orders")
 public class OrderResource {
 
+    private OrderService orderService = new OrderService();
+
     @GET
     @Produces(MediaType.APPLICATION_JSON)
     public Collection<Order> getOrders() {
         //return new OrderDao().findOrders();
-        return new OrderService().getOrders();
+        return orderService.getOrders();
+    }
+
+    @POST
+    @Produces(MediaType.APPLICATION_JSON)
+    public Order createOrder(@NotNull Order order) throws ServerErrorException  {
+        try {
+            return orderService.saveOrder(order);
+        } catch (OrderProcessingException exception) {
+            throw new ServerErrorException(Response.Status.INTERNAL_SERVER_ERROR);
+        }
     }
 }
