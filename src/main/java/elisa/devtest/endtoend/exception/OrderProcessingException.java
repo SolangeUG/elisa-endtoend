d8ec0ef7c03cde595177486aada11828780ccccd
Solange Umuhire GASENGAYIRE <sgasengayire@gmail.com>
2018-05-31 16:07:06 +0200

    Add new constructor and replace use of string values with constants

--- src/main/java/elisa/devtest/endtoend/exception/OrderProcessingException.java
+++ src/main/java/elisa/devtest/endtoend/exception/OrderProcessingException.java
@@ -1,12 +1,27 @@
 package elisa.devtest.endtoend.exception;
 
+import elisa.devtest.endtoend.util.Messages;
+
 /**
  * A class to represent order processing exceptions
  * @author Solange U. Gasengayire
  */
 public class OrderProcessingException extends RuntimeException {
 
+    /**
+     * Constructor
+     * @param e wrapped exception
+     */
     public OrderProcessingException(Exception e) {
-        super("Error processing order", e);
+        super(Messages.ORDER_PROCESSING_EXCEPTION, e);
+    }
+
+    /**
+     * Constructor
+     * @param message exception message
+     * @param e wrapped exception
+     */
+    public OrderProcessingException(String message, Exception e) {
+        super(message, e);
     }
 }
