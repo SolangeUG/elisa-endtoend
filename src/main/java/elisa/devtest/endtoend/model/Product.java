9c86e914c3e667d5c20cbb032304f2aa07c8f74f
Solange Umuhire GASENGAYIRE <sgasengayire@gmail.com>
2018-07-03 13:21:03 +0200

    Add product class documentation

--- src/main/java/elisa/devtest/endtoend/model/Product.java
+++ src/main/java/elisa/devtest/endtoend/model/Product.java
@@ -1,16 +1,29 @@
 package elisa.devtest.endtoend.model;
 
+/**
+ * A bean class to represent a product information
+ */
 public class Product {
     private final String id;
     private final Long priceId;
     private final String name;
     private final String description;
 
-    // Empty private constructor for json serialization framework
+    /**
+     * Private constructor
+     * for json serialization framework
+     */
     private Product() {
         this(null, null, null, null);
     }
 
+    /**
+     * Constructor
+     * @param id product identifier
+     * @param priceId price identifier
+     * @param name product name
+     * @param description product description/detail
+     */
     public Product(String id, Long priceId, String name, String description) {
         this.id = id;
         this.priceId = priceId;
@@ -18,18 +31,34 @@ public class Product {
         this.description = description;
     }
 
+    /**
+     * Return product identifier
+     * @return id
+     */
     public String getId() {
         return id;
     }
 
+    /**
+     * Return price identifier
+     * @return priceId
+     */
     public Long getPriceId() {
         return priceId;
     }
 
+    /**
+     * Return product name
+     * @return name
+     */
     public String getName() {
         return name;
     }
 
+    /**
+     * Return product description/detail
+     * @return description
+     */
     public String getDescription() {
         return description;
     }
