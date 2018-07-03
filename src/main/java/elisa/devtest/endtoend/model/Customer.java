16bc1557b8c76f4eb11e03a98a14b925b2304f63
Solange Umuhire GASENGAYIRE <sgasengayire@gmail.com>
2018-07-03 13:08:38 +0200

    Add customer class documentation

--- src/main/java/elisa/devtest/endtoend/model/Customer.java
+++ src/main/java/elisa/devtest/endtoend/model/Customer.java
@@ -1,5 +1,8 @@
 package elisa.devtest.endtoend.model;
 
+/**
+ * A bean class to represent customer information
+ */
 public class Customer {
     private long customerId;
     private String companyName;
@@ -8,6 +11,15 @@ public class Customer {
     private String city;
     private String country;
 
+    /**
+     * Constructor
+     * @param customerId customer identifier
+     * @param companyName company name
+     * @param street street name
+     * @param postalCode postal code
+     * @param city city
+     * @param country country
+     */
     public Customer(long customerId, String companyName, String street, String postalCode, String city, String country) {
         this.customerId = customerId;
         this.companyName = companyName;
@@ -17,6 +29,9 @@ public class Customer {
         this.country = country;
     }
 
+    /**
+     * Default constructor
+     */
     public Customer() {
     }
 
@@ -36,26 +51,50 @@ public class Customer {
         this.country = country;
     }
 
+    /**
+     * Return customer identifier
+     * @return customerId
+     */
     public long getCustomerId() {
         return customerId;
     }
 
+    /**
+     * Return company name
+     * @return companyName
+     */
     public String getCompanyName() {
         return companyName;
     }
 
+    /**
+     * Return street name
+     * @return street
+     */
     public String getStreet() {
         return street;
     }
 
+    /**
+     * Return postal code
+     * @return postalCode
+     */
     public String getPostalCode() {
         return postalCode;
     }
 
+    /**
+     * Return city name
+     * @return city
+     */
     public String getCity() {
         return city;
     }
 
+    /**
+     * Return country name
+     * @return country
+     */
     public String getCountry() {
         return country;
     }
