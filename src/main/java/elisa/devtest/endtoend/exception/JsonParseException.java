e9c8e18f4f6007121039dfb951f6a50bd7d8cbd7
Solange Umuhire GASENGAYIRE <sgasengayire@gmail.com>
2018-07-04 17:34:05 +0200

    Code cleanup and add class documentation

--- src/main/java/elisa/devtest/endtoend/exception/JsonParseException.java
+++ src/main/java/elisa/devtest/endtoend/exception/JsonParseException.java
@@ -1,7 +1,16 @@
 package elisa.devtest.endtoend.exception;
 
+import elisa.devtest.endtoend.util.Messages;
+
+/**
+ * A class to represent JSON parsing exceptions
+ */
 public class JsonParseException extends RuntimeException {
+    /**
+     * Constructor
+     * @param e json parser exception
+     */
     public JsonParseException(Exception e) {
-        super("Error parsing json", e);
+        super(Messages.JSON_PARSING_EXCEPTION, e);
     }
 }
