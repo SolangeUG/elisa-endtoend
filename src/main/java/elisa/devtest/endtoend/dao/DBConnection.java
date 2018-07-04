ce2c67099eb4182ab20a22ab2e646ef28c45aa3a
Solange Umuhire GASENGAYIRE <sgasengayire@gmail.com>
2018-07-04 16:13:48 +0200

    Code cleanup and add class documentation

--- src/main/java/elisa/devtest/endtoend/dao/DBConnection.java
+++ src/main/java/elisa/devtest/endtoend/dao/DBConnection.java
@@ -1,20 +1,36 @@
 package elisa.devtest.endtoend.dao;
 
+import elisa.devtest.endtoend.util.Messages;
 import org.h2.jdbcx.JdbcConnectionPool;
 
 import javax.sql.DataSource;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
+/**
+ * A class to establish a DB connection
+ */
 public class DBConnection {
 
+    private static final Logger LOGGER = Logger.getLogger(DBConnection.class.getName());
     private static DataSource dataSource = null;
 
+    /**
+     * Return application data source
+     * @return data source
+     */
     public static synchronized DataSource getDataSource() {
         if (dataSource == null) {
             try {
-                Class.forName("org.h2.Driver");
-                dataSource = JdbcConnectionPool.create("jdbc:h2:mem:devtest;MODE=Oracle", "sa", "");
+                String driverName = "org.h2.Driver";
+                String dataSourceUser = "sa";
+                String dataSourcePassword = "";
+                String dataSrouceUrl = "jdbc:h2:mem:devtest;MODE=Oracle";
+
+                Class.forName(driverName);
+                dataSource = JdbcConnectionPool.create(dataSrouceUrl, dataSourceUser, dataSourcePassword);
             } catch (Exception e) {
-                e.printStackTrace();
+                LOGGER.log(Level.SEVERE, Messages.DATASOURCE_CONNECTION_EXCEPTION);
             }
         }
 
