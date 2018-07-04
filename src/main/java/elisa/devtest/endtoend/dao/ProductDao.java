b6300e89f21ffb4fe937ecc644f02008c6c7b36f
Solange Umuhire GASENGAYIRE <sgasengayire@gmail.com>
2018-07-04 16:15:19 +0200

    Code cleanup and add class documentation

--- src/main/java/elisa/devtest/endtoend/dao/ProductDao.java
+++ src/main/java/elisa/devtest/endtoend/dao/ProductDao.java
@@ -8,7 +8,15 @@ import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.List;
 
+/**
+ * Product Data Access Object
+ */
 public class ProductDao {
+
+    /**
+     * Return the list of all products
+     * @return products
+     */
     public List<ProductDto> findProducts() {
         JdbcTemplate template = new JdbcTemplate(DBConnection.getDataSource());
         return template.query("select product_group, product_json from product_dump where product_group = ?",
@@ -16,7 +24,18 @@ public class ProductDao {
                                 new ProductDtoMapper());
     }
 
+    /**
+     * Utility class
+     */
     private class ProductDtoMapper implements RowMapper<ProductDto> {
+
+        /**
+         * Map a result set row to a product data transfer object
+         * @param resultSet result set
+         * @param i row
+         * @return product DTO
+         * @throws SQLException in case of a database error
+         */
         @Override
         public ProductDto mapRow(ResultSet resultSet, int i) throws SQLException {
             return new ProductDto(resultSet.getString(1), resultSet.getString(2));
