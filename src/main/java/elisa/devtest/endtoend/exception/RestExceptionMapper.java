93c1bba144e1e88d8525b2890fd558737b20cc77
Solange Umuhire GASENGAYIRE <sgasengayire@gmail.com>
2018-07-04 17:38:34 +0200

    Code cleanup and add class documentation

--- src/main/java/elisa/devtest/endtoend/exception/RestExceptionMapper.java
+++ src/main/java/elisa/devtest/endtoend/exception/RestExceptionMapper.java
@@ -1,6 +1,8 @@
 package elisa.devtest.endtoend.exception;
 
 
+import elisa.devtest.endtoend.util.Messages;
+
 import javax.ws.rs.WebApplicationException;
 import javax.ws.rs.core.Response;
 import javax.ws.rs.ext.ExceptionMapper;
@@ -8,19 +10,32 @@ import javax.ws.rs.ext.Provider;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
+/**
+ * A class to represent REST exceptions
+ */
 @Provider
 public class RestExceptionMapper implements ExceptionMapper<Throwable> {
     private static final Logger log = Logger.getLogger(RestExceptionMapper.class.getName());
 
+    /**
+     * Return exception as a REST response
+     * @param exception exception that occurred
+     * @return response corresponding to exception
+     */
     @Override
     public Response toResponse(Throwable exception) {
         log.log(Level.SEVERE, exception.toString(), exception);
 
         return Response.status(getStatusCode(exception))
-                .entity("Error occurred")
+                .entity(Messages.REST_PROCESSING_EXCEPTION)
                 .build();
     }
 
+    /**
+     * Return status code for given exception
+     * @param exception exception that occurred
+     * @return corresponding status code
+     */
     private int getStatusCode(Throwable exception) {
         if (exception instanceof WebApplicationException) {
             return ((WebApplicationException) exception).getResponse().getStatus();
