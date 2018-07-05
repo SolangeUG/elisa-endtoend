a662391f9b5d921f439f619d810901b616b0ae98
Solange Umuhire GASENGAYIRE <sgasengayire@gmail.com>
2018-07-05 11:36:13 +0200

    Code cleanup and add class documentation

--- src/main/java/elisa/devtest/endtoend/util/PreparedJsonStatement.java
+++ src/main/java/elisa/devtest/endtoend/util/PreparedJsonStatement.java
@@ -6,16 +6,30 @@ import org.springframework.jdbc.core.PreparedStatementCallback;
 import java.sql.PreparedStatement;
 import java.sql.SQLException;
 
+/**
+ * Class to represent precompiled JSON SQL statement
+ */
 public class PreparedJsonStatement implements PreparedStatementCallback<Boolean> {
     private final String groupIdentifier;
     private final String json;
 
-    public PreparedJsonStatement(String groupIdentifier, String json) {
+    /**
+     * Constructor
+     * @param groupIdentifier entry identifier
+     * @param json JSON content
+     */
+    PreparedJsonStatement(String groupIdentifier, String json) {
         this.groupIdentifier = groupIdentifier;
         this.json = json;
     }
 
-
+    /**
+     * Return result of prepared statement execution
+     * @param preparedStatement provided prepared statement
+     * @return result of execution
+     * @throws SQLException in case of SQL error
+     * @throws DataAccessException data access API agnostic exception
+     */
     @Override
     public Boolean doInPreparedStatement(PreparedStatement preparedStatement) throws SQLException, DataAccessException {
         preparedStatement.setString(1, groupIdentifier);
