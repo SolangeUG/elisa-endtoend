8e731e5f326f430c2c3ba2a7a33df55a2550cd2b
Solange Umuhire GASENGAYIRE <sgasengayire@gmail.com>
2018-07-05 11:31:04 +0200

    Code cleanup and add class documentation

--- src/main/java/elisa/devtest/endtoend/util/DbBootstrap.java
+++ src/main/java/elisa/devtest/endtoend/util/DbBootstrap.java
@@ -2,7 +2,6 @@ package elisa.devtest.endtoend.util;
 
 
 import elisa.devtest.endtoend.dao.DBConnection;
-import elisa.devtest.endtoend.util.PreparedJsonStatement;
 import org.apache.commons.io.IOUtils;
 import org.springframework.jdbc.core.JdbcTemplate;
 
@@ -10,7 +9,14 @@ import java.io.IOException;
 import java.io.InputStream;
 import java.util.Scanner;
 
+/**
+ * Utility class to bootstrap datasource
+ */
 public class DbBootstrap {
+
+    /**
+     * Load datasource and insert initial data
+     */
     public void bootstratp() {
         executeWith("base.sql");
         if(System.getProperty("insertExampleData") != null) {
@@ -20,6 +26,10 @@ public class DbBootstrap {
         executeWithJson("pricing_dump", "PHONES", "pricing.json");
     }
 
+    /**
+     * Execute SQL statements from given file
+     * @param fileName SQL input file
+     */
     private void executeWith(final String fileName) {
         InputStream in = this.getClass().getClassLoader().getResourceAsStream(fileName);
         Scanner scanner = new Scanner(in, "UTF-8").useDelimiter("\\A");
@@ -28,13 +38,19 @@ public class DbBootstrap {
         jdbcTemplate.execute(sql);
     }
 
+    /**
+     * Insert JSON input content into specified table
+     * @param table table name
+     * @param groupIdentifier new entry identifier
+     * @param jsonFileName JSON input file
+     */
     private void executeWithJson(final String table, final String groupIdentifier, final String jsonFileName) {
         InputStream in = this.getClass().getClassLoader().getResourceAsStream(jsonFileName);
-        String json = null;
+        String json;
         try {
             json = IOUtils.toString(in);
         } catch (IOException e) {
-            throw new RuntimeException("Failed to load json files into database", e);
+            throw new RuntimeException(Messages.DATASOURCE_JSON_INSERTION_EXCEPTION, e);
         }
         String statement = "INSERT INTO " + table + " VALUES (?, ?);";
         JdbcTemplate jdbcTemplate = new JdbcTemplate(DBConnection.getDataSource());
