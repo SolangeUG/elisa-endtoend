55f12200ac3ab0b1321126a05eceb6a74e9b4efd
Solange Umuhire GASENGAYIRE <sgasengayire@gmail.com>
2018-07-05 11:05:47 +0200

    Code cleanup and add class documentation

--- src/main/java/elisa/devtest/endtoend/resource/EchoResource.java
+++ src/main/java/elisa/devtest/endtoend/resource/EchoResource.java
@@ -1,16 +1,25 @@
 package elisa.devtest.endtoend.resource;
 
+import elisa.devtest.endtoend.util.Messages;
+
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
 
+/**
+ * Class to represent "echo" REST resource
+ */
 @Path("echo")
 public class EchoResource {
 
+    /**
+     * Process a GET request to this resource
+     * @return echo resource
+     */
     @GET
     @Produces(MediaType.TEXT_PLAIN)
     public String getIt() {
-        return "ping! pong!";
+        return Messages.REST_ECHO_RESOURCE_GET_MESSAGE;
     }
 }
